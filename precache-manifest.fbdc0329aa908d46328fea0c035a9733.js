9b58fadb236d370492dfde14b909a0436aad1697
benamatare <ben.a.matare@gmail.com>
2018-12-11 12:55:16 -0500

    Updates

---
+++ precache-manifest.fbdc0329aa908d46328fea0c035a9733.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed1bfd538a43ef8192e5",
+    "url": "matare.info/static/css/main.b712e116.chunk.css"
+  },
+  {
+    "revision": "ed1bfd538a43ef8192e5",
+    "url": "matare.info/static/js/main.ed1bfd53.chunk.js"
+  },
+  {
+    "revision": "ee1301ba92872effb54d",
+    "url": "matare.info/static/js/1.ee1301ba.chunk.js"
+  },
+  {
+    "revision": "a8268a4afb5c1f60b475",
+    "url": "matare.info/static/js/runtime~main.a8268a4a.js"
+  },
+  {
+    "revision": "e14294d5ecab2940a8c5dccc99c9dba4",
+    "url": "matare.info/static/media/profile_desktop.e14294d5.png"
+  },
+  {
+    "revision": "f95e87554001787f16ad64377293591e",
+    "url": "matare.info/index.html"
+  }
+];
